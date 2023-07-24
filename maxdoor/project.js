f2940e77eb53ab6d431a4e91f07a68cc69e14944
reskinobert <115645104+reskinobert@users.noreply.github.com>
2023-07-24 08:17:27 +0700

    Add files via upload

---
+++ maxdoor/project.js
@@ -0,0 +1,510 @@
+
+// card project list---------------------------------------------------------------------------------
+
+const apiResult = [
+    // -------------------------------RS/School------------------------//
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Advent - Palangkaraya.jpg",
+        name: "Rumah Sakit Advent <br> - Palangkaraya"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Bahteramas - Palu.jpg",
+        name: "Rumah Sakit Bahteramas <br>- Palu  "
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Bhayangkara - Bali.jpg",
+        name: "Rumah Sakit Bhayangkara <br>- Bali"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Bhayangkara - Surabaya.jpg",
+        name: "Rumah Sakit Bhayangkara <br>- Surabaya"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Brawijaya - Jakarta.jpg",
+        name: "Rumah Sakit Brawijaya <br>- Jakarta"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Dr. Iskak - Tulungagung.jpg",
+        name: "Rumah Sakit Dr. Iskak <br>- Tulungagung"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Dr. Soekandar - Mojokerto.jpg",
+        name: "Rumah Sakit Dr. Soekandar <br>- Mojokerto"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Dr. Sutomo - Surabaya.jpg",
+        name: "Rumah Sakit Dr. Sutomo <br>- Surabaya"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Elizabeth - Purwokerto.jpg",
+        name: "Rumah Sakit Elizabeth <br>- Purwokerto"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Haji - Surabaya.jpg",
+        name: "Rumah Sakit Haji <br>- Surabaya"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Jakabaring - Palembang.jfif",
+        name: "Rumah Sakit Jakabaring <br>- Palembang"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Masohi - Maluku.png",
+        name: "Rumah Sakit Masohi <br>- Maluku"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Menur - Surabaya.jpg",
+        name: "Rumah Sakit Menur <br>- Surabaya"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Muhammadiyah - Gresik.png",
+        name: "Rumah Sakit Muhammadiyah <br>- Gresik"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Santa Maria - Cilacap.png",
+        name: "Rumah Sakit Santa Maria <br>- Cilacap"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Siloam - Kupang.jpg",
+        name: "Rumah Sakit Siloam <br>- Kupang"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Siloam - Purwokerto.jpg",
+        name: "Rumah Sakit Siloam <br>- Purwokerto"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Srengat - Blitar.jpg",
+        name: "Rumah Sakit Srengat <br>- Blitar"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Sudiono - Madiun.jpg",
+        name: "Rumah Sakit Sudiono <br>- Madiun"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Umum - Kolaka.jpg",
+        name: "Rumah Sakit Umum <br>- Kolaka"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Rumah Sakit Umum Daerah - Bogor.jpg",
+        name: "Rumah Sakit Umum Daerah <br>- Bogor"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Sekolah Badan Intelijen Nasional - Bogor.png",
+        name: "Sekolah Badan Intelijen Nasional <br>- Bogor"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Sekolah Cung Cung - Surabaya.jpg",
+        name: "Sekolah Cung Cung <br>- Surabaya"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Sekolah Politeknik - Madiun.jpg",
+        name: "Sekolah Politeknik <br>- Madiun"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Sekolah Politeknik - Madura.jpg",
+        name: "Sekolah Politeknik <br>- Madura"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Sekolah Sd Negeri Pakel - Yogyakarta.jpg",
+        name: "Sekolah Sd Negeri Pakel <br>- Yogyakarta"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Sekolah Trinitas - Bandar Lampung.jpg",
+        name: "Sekolah Trinitas <br>- Bandar Lampung"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Universitas Diponegoro - Jepara.jpg",
+        name: "Universitas Diponegoro <br>- Jepara"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Universitas Gadjah Mada - Yogyakarta.jpg",
+        name: "Universitas Gadjah Mada <br>- Yogyakarta"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Universitas Islam Negeri - Tulungagung.jpg",
+        name: "Universitas Islam Negeri <br>- Tulungagung"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Universitas Negeri - Yogyakarta.jpg",
+        name: "Universitas Negeri <br>- Yogyakarta"
+      },
+      {
+        title: "hospital",
+        img: "./images/Project-gedung/rs,school/Universitas Riau - Sumatera.jpeg",
+        name: "Universitas Riau <br>- Sumatera"
+      },
+  
+      // -------------------------------Residences------------------------//
+      {
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Alam Juanda - Surabaya.jpg",
+        name: "Perumahan Alam Juanda <br>- Surabaya"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Chalidana - Surabaya.jpg",
+        name: "Perumahan Chalidana <br>- Surabaya"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Emerald Terrace - Beaksi.jpg",
+        name: "Perumahan Emerald Terrace <br>- Beaksi"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Grand Bale Agung - Pekalongan.png",
+        name: "Perumahan Grand Bale Agung <br>- Pekalongan"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Grand Salt Village - Sidoarjo.jpg",
+        name: "Perumahan Grand Salt Village <br>- Sidoarjo"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Grandwiz - Beaksi.jfif",
+        name: "Perumahan Grandwiz <br>- Beaksi"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Greenland - Kediri.jpg",
+        name: "Perumahan Greenland <br>- Kediri"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Harapan Indah - Bekasi.jpeg",
+        name: "Perumahan Harapan Indah <br>- Bekasi"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Harmoni Park - Cileungsi.jpg",
+        name: "Perumahan Harmoni Park <br>- Cileungsi"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Harmoni Park - Citayam.jpg",
+        name: "Perumahan Harmoni Park <br>- Citayam"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Harmoni Primavera - Bogor.jpg",
+        name: "Perumahan Harmoni Primavera <br>- Bogor"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Kepuh Persuade - Karawang.jpg",
+        name: "Perumahan Kepuh Persuade <br>- Karawang"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Lingkar Mahkota - Lombok.jpg",
+        name: "Perumahan Lingkar Mahkota <br>- Lombok"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Lotus Regency - Banjarmasin.jpg",
+        name: "Perumahan Lotus Regency <br>- Banjarmasin"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Mutiara Puri Harmoni - Bekasi.jpeg",
+        name: "Perumahan Mutiara Puri Harmoni <br>- Bekasi"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Neo Cyprus - Depok.jpg",
+        name: "Perumahan Neo Cyprus <br>- Depok"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Neocasa - Karawang.jpg",
+        name: "Perumahan Neocasa <br>- Karawang"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Pandawa Residence - Yogyakarta.jpg",
+        name: "Perumahan Pandawa Residence <br>- Yogyakarta"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Parkvile Gunung Sindur - Bogor.jpg",
+        name: "Perumahan Parkvile Gunung Sindur <br>- Bogor"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Patra Residence - Cirebon.jpg",
+        name: "Perumahan Patra Residence <br>- Cirebon"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Patra Residence - Yogyakarta.jpg",
+        name: "Perumahan Patra Residence <br>- Yogyakarta"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Podomoro Tenjo - Bogor.jpg",
+        name: "Perumahan Podomoro Tenjo <br>- Bogor"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Puri Harmoni - Cisoka.jpeg",
+        name: "Perumahan Puri Harmoni <br>- Cisoka"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Puri Harmoni - Muktiwari.jpeg",
+        name: "Perumahan Puri Harmoni <br>- Muktiwari"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Puri Harmoni Indah - Serang.jpg",
+        name: "Perumahan Puri Harmoni Indah <br>- Serang"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung//residence/Perumahan Tamaruma - Depok.jpg",
+        name: "Perumahan Tamaruma <br>- Depok"
+      },{
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan The Leaves - Tangerang.jpg",
+        name: "Perumahan The Leaves <br>- Tangerang"
+      },
+      {
+        title: "residence",
+        img: "./images/Project-gedung/residence/Perumahan Wahid - Semarang.jpg",
+        name: "Perumahan Wahid <br>- Semarang"
+      },
+  
+      // -------------------------------hotel------------------------//
+  
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Apartment B Residence - Bogor.jpeg",
+        name: "Apartment B Residence <br>- Bogor"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Apartment Baileys - Jakarta.jpg",
+        name: "Apartment Baileys <br>- Jakarta"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Apartment Havenwood - SImatupang.jpg",
+        name: "Apartment Havenwood <br>- SImatupang"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Apartment Malioboro City - Yogyakarta.jpg",
+        name: "Apartment Malioboro City <br>- Yogyakarta"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Apartment Taman Melati - Surabaya.jpg",
+        name: "Apartment Taman Melati <br>- Surabaya"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Anugerah Palace - Solo.jpg",
+        name: "Hotel Anugerah Palace <br>- Solo"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Aria - Surabaya.jpg",
+        name: "Hotel Aria <br>- Surabaya"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Aston - Madiun.jpg",
+        name: "Hotel Aston <br>- Madiun"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Aston Inn - Palu.jpg",
+        name: "Hotel Aston Inn <br>- Palu"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Celvasha - Jakarta.jpg",
+        name: "Hotel Celvasha <br>- Jakarta"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel D’Season - Jepara.jfif",
+        name: "Hotel D’Season <br>- Jepara"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Fave - Banjarmasin.jpg",
+        name: "Hotel Fave <br>- Banjarmasin"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Front One - Pamekasan.jpg",
+        name: "Hotel Front One <br>- Pamekasan"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Gran Wahid - Salatiga.png",
+        name: "Hotel Gran Wahid <br>- Salatiga"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Inside By Melia - Yogyakarta.jpg",
+        name: "Hotel Inside By Melia <br>- Yogyakarta"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Kuningan - Jakarta.jpg",
+        name: "Hotel Kuningan <br>- Jakarta"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Life Style - Surabaya.jpg",
+        name: "Hotel Life Style <br>- Surabaya"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel My Studio - Sidoarjo.jpg",
+        name: "Hotel My Studio <br>- Sidoarjo"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Plaza Inn - Kendari.jpg",
+        name: "Hotel Plaza Inn <br>- Kendari"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Rama - Palu.jpg",
+        name: "Hotel Rama <br>- Palu"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Suite Ijen - Malang.jpg",
+        name: "Hotel Suite Ijen <br>- Malang"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel The Sun - Madiun.jpg",
+        name: "Hotel The Sun <br>- Madiun"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Hotel Win - Surabaya.jpg",
+        name: "Hotel Win <br>- Surabaya"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Oﬃce Gubernur - Manokwari.jpg",
+        name: "Oﬃce Gubernur <br>- Manokwari"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Oﬃce Inspektorat - Solo.jpg",
+        name: "Oﬃce Inspektorat <br>- Solo"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Oﬃce Pelindo - Perak.png",
+        name: "Oﬃce Pelindo <br>- Perak"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Oﬃce Plaza Oleos - Jakarta.jpg",
+        name: "Oﬃce Plaza Oleos <br>- Jakarta"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Rusun Asrama Haji - Indramayu.jpg",
+        name: "Rusun Asrama Haji <br>- Indramayu"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Rusun Asrama UNY - Yogyakarta.jpeg",
+        name: "Rusun Asrama UNY <br>- Yogyakarta"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Rusun BWS - Limbung.jpg",
+        name: "Rusun BWS <br>- Limbung"
+      },
+      {
+        title: "hotel",
+        img: "./images/Project-gedung/hotel,tower/Rusun Mataram - NTT.jpg",
+        name: "Rusun Mataram <br>- NTT"
+      }
+    ];
+      
+      
+      const container = document.getElementById('mix-container');
+      
+      apiResult.forEach((result, idx) => {
+        // Create card element
+        const card = document.createElement('div');
+        card.classList = 'img-cust';
+      
+        // Construct card content
+        const content = `
+          
+              <div class="card ${result.title}" id="slider">
+                  <img src="${result.img}" 
+                      alt="#"
+                      class="img-responsive"
+                      width="500px"
+                  />
+                  <div class="figcaption">
+                      <h3 style="font-size: .8em;">${result.name}</h3>
+                  </div>
+              </div>               
+          
+        `;
+      
+        // Append newyly created card element to the container
+        container.innerHTML += content;
+      })
+    
+
+      function myapp() {
+        const buttons = document.querySelectorAll(".button");
+        const cards = document.querySelectorAll(".card");
+      
+        function filter(category, items) {
+          items.forEach((item) => {
+            const isItemFiltered = !item.classList.contains(category);
+            const isShowAll = category.toLowerCase() === "all";
+            if (isItemFiltered && !isShowAll) {
+              item.classList.add("hide");
+            } else {
+              item.classList.remove("hide");
+            }
+          });
+        }
+      
+        buttons.forEach((button) => {
+          button.addEventListener("click", () => {
+            const currentCategory = button.dataset.filter;
+            console.log(currentCategory);
+            filter(currentCategory, cards);
+          });
+        });
+      }
+      
+      myapp();
